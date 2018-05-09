1960449c28c9eb15403a669ca29ee900d637e46e
zc402 <zc35402@163.com>
2018-05-09 23:32:19 +0800

    Fix bug on webcam.
    Add Rnn state saving. Run rnn_webcam.py to see the result!

--- rnn_webcam.py
+++ rnn_webcam.py
@@ -1,3 +1,4 @@
+
 import parameters as pa
 import tensorflow as tf
 import gpu_network
@@ -15,6 +16,7 @@ def main(argv=None):
         img_holder = tf.placeholder(tf.float32, [batch_size, pa.PH, pa.PW, 3])
         # Entire network
         paf_pcm_tensor = gpu_network.PoseNet().inference_paf_pcm(img_holder)
+        saver = tf.train.Saver()
 
     with g_2.as_default():
         NUM_CLASSES = 9  # 8 classes + 1 for no move
@@ -22,22 +24,27 @@ def main(argv=None):
 
         # batch_time_joint_holder:
         btjh = tf.placeholder(tf.float32, [1, 1, NUM_JOINTS, 2])  # 2:xy
+        c_state_holder = tf.placeholder(tf.float32, [32])
+        h_state_holder = tf.placeholder(tf.float32, [32])
+        
+        state_tuple = ([c_state_holder], [h_state_holder])
+        
         with tf.variable_scope("rnn-net"):
             # b t c(0/1)
             img_j_xy = tf.reshape(btjh, [-1, NUM_JOINTS, 2])
             img_fe = rnn_network.extract_features_from_joints(img_j_xy)
             btf = tf.reshape(img_fe, [1, 1, -1])
-            pred, state = rnn_network.build_rnn_network(btf, NUM_CLASSES)
+            pred, state = rnn_network.build_rnn_network(btf, NUM_CLASSES, state_tuple)
             # model evaluation
             btc_pred = tf.transpose(pred, [1, 0, 2])  # TBC to BTC
             btc_pred_max = tf.argmax(btc_pred, 2)
+            rnn_saver = tf.train.Saver()
 
 
     # Session Saver
     sess1 = tf.Session(graph=g_1)
     sess2 = tf.Session(graph=g_2)
 
-    saver = tf.train.Saver()
     ckpt = tf.train.get_checkpoint_state("logs/")
     if ckpt:
         saver.restore(sess1, ckpt.model_checkpoint_path)
@@ -46,20 +53,28 @@ def main(argv=None):
 
     rnn_ckpt = tf.train.get_checkpoint_state("rnn_logs/")
     if rnn_ckpt:
-        saver.restore(sess2, rnn_ckpt.model_checkpoint_path)
+        rnn_saver.restore(sess2, rnn_ckpt.model_checkpoint_path)
     else:
         raise FileNotFoundError("RNN ckpt not found.")
 
     cap = cv2.VideoCapture(0)
-    cap.set(cv2.CV_CAP_PROP_FRAME_WIDTH,512);
-    cap.set(cv2.CV_CAP_PROP_FRAME_HEIGHT,512);
+    # cap.set(3 ,512)
+    # cap.set(4, 512)
 
-    while(True):
+    rnn_saved_state = None
+    while cap.isOpened():
         ret, frame = cap.read()
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        cv2.imshow('PolicePose', frame)
+        z = np.zeros([512, 512, 3], dtype=np.uint8)
+        z[0:480, 0:512, :] = frame[0:480, 0:512, :]
+        
+        cv2.imshow('PolicePose', z)
+        cv2.waitKey(5)
+        
+        rgb = cv2.cvtColor(z, cv2.COLOR_BGR2RGB)
+        rgb = rgb.astype(np.float32)
         rgb_norm = rgb / 255.
-
+        rgb_norm = rgb_norm[np.newaxis]
+        
         feed_dict = {img_holder: rgb_norm}
         paf_pcm = sess1.run(paf_pcm_tensor, feed_dict=feed_dict)
         pcm = paf_pcm[:, :, :, 14:]
@@ -68,7 +83,7 @@ def main(argv=None):
         # 6 joint in image
         img_j6 = []
         for idx_joint in range(8):
-            heat = pcm[1, :, :, idx_joint]
+            heat = pcm[0, :, :, idx_joint]
             c_coor_1d = np.argmax(heat)
             c_coor_2d = np.unravel_index(
                 c_coor_1d, [pa.HEAT_SIZE[1], pa.HEAT_SIZE[0]])
@@ -96,12 +111,16 @@ def main(argv=None):
                 7: "SLOW DOWN",
                 8: "GET OFF"}
         # video_utils.save_joints_position(v_name)
-        joint_data = img_j6[np.newaxis, np.newaxis, :, :]
-        feed_dict = {btjh: joint_data}
-        btc_pred_num = sess2.run(btc_pred_max, feed_dict=feed_dict)
+        joint_data = np.asarray(img_j6)
+        joint_data = joint_data[np.newaxis, np.newaxis, :, :]
+        
+        if rnn_saved_state is None:
+            rnn_saved_state = ([np.zeros(32), np.zeros(32)])
+        feed_dict = {btjh: joint_data, c_state_holder: rnn_saved_state[0], h_state_holder: rnn_saved_state[1]}
+        btc_pred_num, state_num = sess2.run([btc_pred_max, state], feed_dict=feed_dict)
+        rnn_saved_state = ([state_num[0][0], state_num[1][0]])
         pred = np.reshape(btc_pred_num, [-1])
-        assert(len(pred) == 0)
-        print(police_dict(pred[0]))
+        print(police_dict[pred[0]])
 
     cap.release()
     cv2.destroyAllWindows()
